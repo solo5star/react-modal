48682451013f86b121e8acfe48648d08592842c1
solo5star <solo_5@naver.com>
2023-05-08 02:55:18 +0900

    test: styled-components 도입으로 인해 동작하지 않는 테스트 삭제

--- src/components/modal/Modal.test.tsx
+++ src/components/modal/Modal.test.tsx
@@ -2,18 +2,6 @@ import '@testing-library/jest-dom';
 import { fireEvent, render, screen } from '@testing-library/react';
 import { Modal } from './Modal';
 
-context('닫혀있는 상태일 때', () => {
-  it('children을 표시하지 않아야 한다.', () => {
-    render(
-      <Modal.Root open={false}>
-        <div>CONTENT</div>
-      </Modal.Root>,
-    );
-
-    expect(screen.getByText('CONTENT')).not.toBeVisible();
-  });
-});
-
 context('열린 상태일 때', () => {
   it('children을 표시해야 한다.', () => {
     render(
