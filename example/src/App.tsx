a8a2033899d5f681a5978683cd4c12599d17d953
solo5star <solo_5@naver.com>
2023-05-08 02:11:32 +0900

    feat: 예제 프로젝트 추가

---
+++ example/src/App.tsx
@@ -0,0 +1,81 @@
+import { useState } from 'react';
+import { Modal } from 'react-modal';
+import { styled } from 'styled-components';
+import './App.css';
+
+const MyModal = {
+  ...Modal,
+  Content: styled(Modal.Content)`
+    background: hsl(0, 0%, 16%);
+    border-radius: 8px;
+
+    @media (prefers-color-scheme: light) {
+      background: hsl(0, 0%, 100%);
+    }
+  `,
+};
+
+const ModalPlacementPanel = styled.div`
+  display: inline-grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 4px;
+`;
+
+const ToTop = styled.button`
+  grid-area: 1 / 2 / 1 / 2;
+`;
+
+const ToBottom = styled.button`
+  grid-area: 3 / 2 / 3 / 2;
+`;
+
+const ToLeft = styled.button`
+  grid-area: 2 / 1 / 2 / 1;
+`;
+
+const ToRight = styled.button`
+  grid-area: 2 / 3 / 2 / 3;
+`;
+
+const ToCenter = styled.button`
+  grid-area: 2 / 2 / 2 / 2;
+`;
+
+type Placement = React.ComponentProps<typeof MyModal.Content>['placement'];
+
+function App() {
+  const [modalOpen, setModalOpen] = useState(false);
+  const [modalPlacement, setModalPlacement] = useState<Placement>('center');
+
+  const handleModalOpen = () => setModalOpen(true);
+  const handleModalClose = () => setModalOpen(false);
+
+  return (
+    <>
+      <main>
+        <h1>Here is a modal example!</h1>
+        <p>👇 By clicking the button, pay close attention to what happens!</p>
+        <button onClick={handleModalOpen}>Open Modal</button>
+      </main>
+
+      <MyModal.Root open={modalOpen}>
+        <MyModal.Backdrop onClick={handleModalClose} />
+        <MyModal.Content placement={modalPlacement}>
+          <h1>Whoa! 🎉 The wild modal has appeared! ✨</h1>
+          <p>It's small and lightweight, but also the beginning of a great journey!</p>
+
+          <ModalPlacementPanel>
+            <ToTop onClick={() => setModalPlacement('top')}>Top</ToTop>
+            <ToBottom onClick={() => setModalPlacement('bottom')}>Bottom</ToBottom>
+            <ToLeft onClick={() => setModalPlacement('left')}>Left</ToLeft>
+            <ToRight onClick={() => setModalPlacement('right')}>Right</ToRight>
+            <ToCenter onClick={() => setModalPlacement('center')}>Center</ToCenter>
+          </ModalPlacementPanel>
+        </MyModal.Content>
+      </MyModal.Root>
+    </>
+  );
+}
+
+export default App;
+
