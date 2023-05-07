3d484efbd8e0dc4fead12db646d89974d7c79bf4
solo5star <solo_5@naver.com>
2023-05-07 15:48:33 +0900

    chore: rollup + esbuild + typescript 번들러 설정

---
+++ rollup.config.js
@@ -0,0 +1,39 @@
+import dts from "rollup-plugin-dts";
+import esbuild from "rollup-plugin-esbuild";
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import prettier from "rollup-plugin-prettier";
+import pkg from "./package.json" assert { type: "json" };
+
+/** @type {import('rollup').RollupOptions} */
+export default [
+  {
+    input: "src/index.tsx",
+    output: [
+      {
+        file: pkg.exports["."].import,
+        format: "esm",
+      },
+      {
+        file: pkg.exports["."].require,
+        format: "cjs",
+      },
+    ],
+    plugins: [
+      esbuild({
+        include: /\.[jt]sx?$/,
+        minify: process.env.NODE_ENV === "production",
+      }),
+      peerDepsExternal(),
+    ],
+  },
+  {
+    input: "src/index.tsx",
+    output: [
+      {
+        file: pkg.types,
+        format: "cjs",
+      },
+    ],
+    plugins: [dts(), prettier({ tabWidth: 2 })],
+  },
+];
