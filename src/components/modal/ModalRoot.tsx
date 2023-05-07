2d949497a5613a059e17d7a7e9bb3cd0f2209786
solo5star <solo_5@naver.com>
2023-05-08 04:30:58 +0900

    fix: 잘못된 position 수정 및 z-index 위로 끌어올리기

--- src/components/modal/ModalRoot.tsx
+++ src/components/modal/ModalRoot.tsx
@@ -10,11 +10,12 @@ type RootProps = {
 const Root = styled.div<RootProps>`
   display: ${(props) => (props.$open ? 'grid' : 'none')};
 
-  position: absolute;
+  position: fixed;
   top: 0;
   left: 0;
   right: 0;
   bottom: 0;
+  z-index: 1000;
 
   & > * {
     grid-area: 1 / 1 / 1 / 1;
