9b6630a7a4ad27ce8962f8d263f52e776ea0a5dc
solo5star <solo_5@naver.com>
2023-05-07 16:39:11 +0900

    style: rollup.config.js 코드 포맷팅

--- rollup.config.js
+++ rollup.config.js
@@ -1,37 +1,37 @@
-import dts from "rollup-plugin-dts";
-import esbuild from "rollup-plugin-esbuild";
-import peerDepsExternal from "rollup-plugin-peer-deps-external";
-import prettier from "rollup-plugin-prettier";
-import pkg from "./package.json" assert { type: "json" };
+import dts from 'rollup-plugin-dts';
+import esbuild from 'rollup-plugin-esbuild';
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import prettier from 'rollup-plugin-prettier';
+import pkg from './package.json' assert { type: 'json' };
 
 /** @type {import('rollup').RollupOptions} */
 export default [
   {
-    input: "src/index.tsx",
+    input: 'src/index.tsx',
     output: [
       {
-        file: pkg.exports["."].import,
-        format: "esm",
+        file: pkg.exports['.'].import,
+        format: 'esm',
       },
       {
-        file: pkg.exports["."].require,
-        format: "cjs",
+        file: pkg.exports['.'].require,
+        format: 'cjs',
       },
     ],
     plugins: [
       esbuild({
         include: /\.[jt]sx?$/,
-        minify: process.env.NODE_ENV === "production",
+        minify: process.env.NODE_ENV === 'production',
       }),
       peerDepsExternal(),
     ],
   },
   {
-    input: "src/index.tsx",
+    input: 'src/index.tsx',
     output: [
       {
         file: pkg.types,
-        format: "cjs",
+        format: 'cjs',
       },
     ],
     plugins: [dts(), prettier({ tabWidth: 2 })],
